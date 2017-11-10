58ac5f3e3144fd7ff343762bd482ca31e2e78247
Peter Wildberger <pete.wildberger@gmail.com>
2017-11-10 12:30:38 -0600

    still need some work

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: path.resolve(__dirname, "target") + "/classes/static/built/",
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                include: path.resolve(__dirname, "src"),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  entry: './src/main/js/app.js',
+  devtool: 'sourcemaps',
+  cache: true,
+  output: {
+    path: path.resolve(__dirname, 'target') + '/classes/static/built/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
 };
